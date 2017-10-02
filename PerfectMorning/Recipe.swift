136333dcdb13d226df8ee38a00faeb18a66a1bae
Julia <berrylily.0709@gmail.com>
2017-10-02 14:42:41 -0700

    Rebase to the latest master

---
+++ PerfectMorning/Recipe.swift
@@ -0,0 +1,20 @@
+//
+//  Recipe.swift
+//  PerfectMorning
+//
+//  Created by Julia on 2017/09/29.
+//  Copyright © 2017 岡田暁. All rights reserved.
+//
+
+import UIKit
+
+struct Recipe {
+    var recipeId: String
+    var socialRank: Int
+    var f2fUrl: String
+    var title: String
+    var imageUrl: String
+    var publisher: String
+    var publisherUrl: String
+    var sourceUrl: String
+}
