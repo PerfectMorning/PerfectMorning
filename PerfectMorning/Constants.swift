46791e90aa6febe3c6713e809ce5b8ffadbd857e
aaakiii <a.okada3@gmail.com>
2017-10-02 12:03:12 -0700

    constants changed

---
+++ PerfectMorning/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  PerfectMorning
+//
+//  Created by 岡田暁 on 2017-09-29.
+//  Copyright © 2017 岡田暁. All rights reserved.
+//
+
+struct Constants {
+    let food2ForkApiKey = "<YOUR API KAEY>"
+    
+}
