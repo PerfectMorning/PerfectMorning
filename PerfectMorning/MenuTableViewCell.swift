6e9095649e73124b52216f479e03e81a803dd34f
aaakiii <a.okada3@gmail.com>
2017-10-04 15:23:58 -0700

    clean code

--- PerfectMorning/MenuTableViewCell.swift
+++ PerfectMorning/MenuTableViewCell.swift
@@ -34,53 +34,12 @@ class MenuTableViewCell: UITableViewCell {
     func setCell(menu: Menu) {
     
         self.menu = menu
-        //        self.menuView.image = darken(image: menu.image)
         self.menuColorImage.backgroundColor = menu.color
         self.menuImage.image = menu.image
-        
-//        menuView.backgroundColor = UIColor.clear
-        menuColorImage.alpha = 0.8
+        self.menuColorImage.alpha = 0.8
         self.menuTitle.text = menu.title
-
-        
-        
-//        .withRenderingMode(.alwaysTemplate)
-//        self.menuView.tintColor = menu.color
-        
-//        self.menuView.alpha = 0.2
-//        self.menuImage.backgroundColor = menu.color
         menuButton.setImage(#imageLiteral(resourceName: "arrow_right"), for: .normal)
         menuButton.tintColor = UIColor.white
 
     }
-    
-//    func darken(image:UIImage) -> UIImage{
-    
-        // 一時的な暗くするようの黒レイヤ
-//        let frame = CGRect(origin:CGPoint(x:0,y:0),size:image.size)
-//        let tempView = UIView(frame:frame)
-//        tempView.backgroundColor = menu?.color
-//        tempView.alpha = 0.8
-        
-       
-        // 画像を新しいコンテキストに描画する
-//        UIGraphicsBeginImageContext(frame.size)
-//        let context = UIGraphicsGetCurrentContext()
-//        alphaImage.draw(in: frame)
-//
-        
-        
-        // コンテキストに黒レイヤを乗せてレンダー
-//        context!.translateBy(x: 0, y: frame.size.height)
-//        context!.scaleBy(x: 1.0, y: -1.0)
-//        context!.clip(to: frame, mask: image.cgImage!)
-//        tempView.layer.render(in: context!)
-//
-//        let imageRef = context!.makeImage()
-//        let toReturn = UIImage(cgImage:imageRef!)
-//        UIGraphicsEndImageContext()
-//        return toReturn
-//
-//    }
-    
 }
--- PerfectMorning/MenuTableViewController.swift
+++ PerfectMorning/MenuTableViewController.swift
@@ -13,17 +13,13 @@ class MenuTableViewController: UIViewController , UITableViewDataSource, UITable
     
     
     @IBOutlet var menuTableView: UITableView!
-    
   
     var menues: [Menu] = []
-//    var genre:[String] = ["Fast", "Arrange", "Heigh", "Elegant"]
-//    var image: [UIImage] = [#imageLiteral(resourceName: "menu_fast"), #imageLiteral(resourceName: "menu_arrange"), #imageLiteral(resourceName: "menu_high"), #imageLiteral(resourceName: "menu_elegant")]
-//    var color:[UIColor] = [UIColor(red:1.00, green:0.80, blue:0.00, alpha:0.8),UIColor(red:1.00, green:0.65, blue:0.00, alpha:0.8),UIColor(red:0.92, green:0.45, blue:0.27, alpha:0.8),UIColor(red:0.95, green:0.35, blue:0.50, alpha:0.8)]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Menu"
-        
         menuTableView.rowHeight = 150
         menuTableView.frame = view.frame
         view.addSubview(menuTableView)
@@ -53,15 +49,7 @@ class MenuTableViewController: UIViewController , UITableViewDataSource, UITable
         let menuCell = menuTableView.dequeueReusableCell(withIdentifier: "MenuTableCell", for: indexPath) as! MenuTableViewCell
         
         menuCell.setCell(menu: menues[indexPath.row])
-        
-//        menuCell.menuImage.image = image[indexPath.row]
-//        menuCell.menuImage.alpha = 0.2
-//
-//        menuCell.menuTitle.text = genre[indexPath.row]
-//        menuCell.image.tintColor = color[indexPath.row]
-//        menuCell.imageView?.image = image[indexPath.row]
-//        menuCell.imageView?.tintColor = color[indexPath.row]
-//        menuCell.textLabel?.text = genre[indexPath.row]
+
 
         return menuCell
     }
