356ea0fe8b4ccbeb3610885cc8d44c6843f383fc
Julia <berrylily.0709@gmail.com>
2017-10-05 14:56:48 -0700

    Tweak bug

--- PerfectMorning/DetailViewController.swift
+++ PerfectMorning/DetailViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var detailView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    
+//    private let headerHeight: CGFloat = 300
+//    var headerImageView = imageView
+
     var selectedRecipe: Recipe!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Initialize and set headerView
+//        detailView.addSubview(headerView)
+//
+//        detailView.contentInset = UIEdgeInsets(top: headerHeight, left: 0, bottom: 0, right: 0)
+//        detailView.contentOffset = CGPoint(x: 0, y: -headerHeight)
+//        updateHeaderView()
 
         let url = URL(string: selectedRecipe.imageUrl)
         let data = try? Data(contentsOf: url!)
@@ -24,8 +36,18 @@ class DetailViewController: UIViewController {
 
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
+//    override func scrollViewDidScroll(scrollView: UIScrollView) {
+//        updateHeaderView()
+//    }
+//
+//    private func updateHeaderView() {
+//        detailView.contentInset = UIEdgeInsets(top: headerHeight, left: 0, buttom: 0, right: 0)
+//        var headerRect = CGRect(x: 0, y: -headerHeight, width: imageView.bounds.width, height: headerHeight)
+//
+//        if detailView.contentOffset.y < -headerHeight {
+//            headerRect.origin.y = detailView.contentOffset.y
+//            headerRect.size.height = -detailView.contentOffset.y
+//        }
+//        headerView.frame = headerRect
+//    }
 }
