c2fa3b1a47914aea3514a378b134997841c1703b
Aki Okada <a.okada3@gmail.com>
2017-10-04 20:48:56 -0700

    Add menu (#9)

---
+++ PerfectMorning/Menu.swift
@@ -0,0 +1,17 @@
+//
+//  Menu.swift
+//  PerfectMorning
+//
+//  Created by 岡田暁 on 2017-10-02.
+//  Copyright © 2017 岡田暁. All rights reserved.
+//
+
+import UIKit
+
+struct Menu {
+    var title: String
+    var image: UIImage
+    var color: UIColor
+  
+}
+
