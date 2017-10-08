95ad28f37d36f658fd7d46237a4566eae6e03201
Julia <berrylily.0709@gmail.com>
2017-10-07 17:30:13 -0700

    Make Groups

---
+++ PerfectMorning/Models/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  PerfectMorning
+//
+//  Created by Julia on 2017/09/29.
+//  Copyright © 2017 Aki. All rights reserved.
+//
+
+import UIKit
+
+struct Recipe {
+    var recipeName: String
+    var totalTimeInSeconds: Int
+    var id: String
+    var imageUrlsBySize: String
+    var ingredients: [String] 
+}
